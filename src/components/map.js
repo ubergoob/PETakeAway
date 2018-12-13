ad5d97576a13c7362e9108af201c3a247c253d42
Paul T Bierly <ubergoober@gmail.com>
2018-12-12 18:05:24 -0600

    fighting async for the additonal data from the Virgina site. although I see data coming back, it won't parse. not sure why the promises aren't following through.. or maybe they are, still debugging

--- src/components/map.js
+++ src/components/map.js
@@ -9,8 +9,19 @@ const mapAttribution = activeTile.attribution;
 const mapCenter = [37.541885, -77.440624];
 const zoomLevel = 13;
 
+let baseIcon = L.icon({
+  iconUrl: require('../markers/fire.png'),
+  iconSize: [32, 37],
+  popupAnchor: [-3, -76]
+})
 
 class Map extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      marker: null
+    }
+  }
 
   componentDidMount() {
     this.map = L.map('map', {
@@ -22,9 +33,44 @@ class Map extends React.Component {
         }),
       ]
     });
+    // adding marker layer
+    this.layer = L.layerGroup().addTo(this.map)
+    // add initial markers
+    this._addMarkers()
   }
 
+  _addMarkers = () => {
+    this.layer.clearLayers();
+    //get markers. Stored in local json files for now:
+    const incident = require('../data/F01705150050.json')
+    // collect some more data on location
+    this._getAdditionalData(incident)
+  }
 
+  _getAdditionalData = function(incident) {
+    let url = 'http://gis.richmondgov.com/ArcGIS/rest/services/StatePlane4502/Ener/MapServer/0/query?geometry=' + incident.address.longitude + ',' + incident.address.latitude + '&geometryType=esriGeometryPoint&inSR=4326&spatialRel=esriSpatialRelIntersects&time=&returnCountOnly=false&returnIdsOnly=false&returnGeometry=true&outFields=*&f=pjson'
+      console.log(url)
+      fetch(url, {
+        crossDomain: true,
+        mode: 'no-cors'
+      })
+      .then(response => response.json())
+      .then(jsonResp => {incident.additionalData = jsonResp; this.setState({marker: incident})})
+      .catch (err => console.log(err))
+      //incident.additionalData = jsonResp
+
+      this._placeMarker(incident)
+    
+  
+  }
+  
+  _placeMarker = (incident) => {
+    console.log("did we attempt this?")
+    console.log(incident)
+    // just addin markers for the moment, will perdy-fy them in a bit.
+    console.log(incident)
+    this.marker = L.marker([incident.address.latitude, incident.address.longitude], {icon: baseIcon}).addTo(this.layer)
+  }
 
   render() {
     return <div id="map" style={{height: '100vh'}}></div>
